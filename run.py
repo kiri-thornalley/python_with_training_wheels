3945afddc863345e0b9b9c476455bf118aaa7654
Kiri Thornalley <143103370+kiri-thornalley@users.noreply.github.com>
2025-05-23 12:42:31 +0100

    chore: modify run to work with Docker, create Dockerfile

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ app = create_app()  # Initialise the app
 app.secret_key = 'BumBlEBee'
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Run the app in debug mode for development
+    app.run(host="0.0.0.0", port=5000, debug=False) 
+    #app.run(debug=True) # Run the app in debug mode for development
